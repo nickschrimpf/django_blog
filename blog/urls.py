6bdeb778642d892374ab20c9f852fa4329abe910
Nicholas Schrimpf <nickschrimpf@users.noreply.github.com>
2023-02-15 10:16:00 -0700

    added dynamic templates for post and post-detail

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.index),
-    # path('posts',views.posts)
+    path('',views.index,name="welcome-page"),
+    path('posts',views.posts_page,name="posts-page"),
+    path('posts/<slug:slug>',views.post_detail,name="post-detail-page")
 ]
